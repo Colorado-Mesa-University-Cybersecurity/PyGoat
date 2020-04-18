742bba5bc54d05850c1ad1b67f533ffd6be10c4a
Taylor Bradshaw <TaylorColeBradshaw@gmail.com>
2020-04-18 11:11:23 -0600

    basic test

--- tests/test_PyGoat.py
+++ tests/test_PyGoat.py
@@ -20,5 +20,38 @@ import main
 #     os.close(db_fd)
 #     os.unlink(flaskr.app.config['DATABASE'])
 
-def test_make_sql_query():
-    assert 4 + 5 == 8
+#Start new database
+try:
+    os.remove('pygoat.db')
+except FileNotFoundError:
+    pass # already removed, do nothing
+
+# conn = sqlite3.connect('pygoat.db')
+# c = conn.cursor()
+# c.execute('''CREATE TABLE if not exists users
+#                  (username text, password blob, salt blob)''')
+# conn.commit()
+
+def test_init_database():
+    #create and initialize empty database in test directory
+    for lesson in main.lessons:
+        main.initialize_db(lesson)
+
+    #create a test user in test database
+    username = 'testBlankUser'
+    password = '12345'
+    
+    #assert that the test user starts with no lesson completions
+    if not main.valid_login(username, password):
+        assert(False)
+
+    #remove database from test directory
+    os.remove('pygoat.db')
+
+    
+
+# def test_make_sql_query():
+#     query
+#     request
+#     main.make_sql_query(query, request)
+    
