555bb07531c6de6a0ea933cbe6dbeb7c2f7de18a
Taylor Bradshaw <TaylorColeBradshaw@gmail.com>
2020-04-18 13:09:18 -0600

    basic test for working database

--- tests/test_PyGoat.py
+++ tests/test_PyGoat.py
@@ -1,7 +1,7 @@
 import os, sys
 import tempfile
 
-import pytest, sqlite3
+import pytest, sqlite3, hashlib
 
 sys.path.append('../')
 import main
@@ -20,13 +20,15 @@ import main
 #     os.close(db_fd)
 #     os.unlink(flaskr.app.config['DATABASE'])
 
+dbname='testing_pygoat.db'
+
 #Start new database
 try:
-    os.remove('pygoat.db')
+    os.remove(dbname)
 except FileNotFoundError:
     pass # already removed, do nothing
 
-conn = sqlite3.connect('pygoat.db')
+conn = sqlite3.connect(dbname)
 c = conn.cursor()
 c.execute('''CREATE TABLE if not exists users
                  (username text, password blob, salt blob)''')
@@ -40,10 +42,23 @@ def test_init_database():
     #create a test user in test database
     username = 'testBlankUser'
     password = '12345'
+
+    #assert that username doesn't exist in table
+    assert(main.valid_login(username, password, dbname=dbname, testing=True) == False)
+
+    salt = os.urandom(32)
+
+    m = hashlib.sha256()
+    m.update(salt)
+    m.update(password.encode('utf-8'))
+    pass_hash = m.digest()
+
+    c.execute('''INSERT INTO users (username, password, salt) VALUES (?, ?, ?)''', (username, pass_hash, salt))
+    conn.commit()
+    conn.close()
     
     #assert that the test user starts with no lesson completions
-    if not main.valid_login(username, password):
-        assert(False)
+    assert(main.valid_login(username, password, dbname=dbname, testing=True) == True)
 
     #remove database from test directory
     os.remove('pygoat.db')
