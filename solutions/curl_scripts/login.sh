e0ee7a524b105979a5b02831c6763e5e42ffbd6a
Lucas Walgren <lwalgren12@gmail.com>
2020-04-25 14:56:51 -0600

    added login.sh

---
+++ solutions/curl_scripts/login.sh
@@ -0,0 +1,14 @@
+curl 'http://localhost:5000/login' \
+  -H 'Connection: keep-alive' \
+  -H 'Pragma: no-cache' \
+  -H 'Cache-Control: no-cache' \
+  -H 'Upgrade-Insecure-Requests: 1' \
+  -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36' \
+  -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9' \
+  -H 'Sec-Fetch-Site: same-origin' \
+  -H 'Sec-Fetch-Mode: navigate' \
+  -H 'Sec-Fetch-User: ?1' \
+  -H 'Sec-Fetch-Dest: document' \
+  -H 'Referer: http://localhost:5000/' \
+  -H 'Accept-Language: en-US,en;q=0.9' \
+  --compressed
