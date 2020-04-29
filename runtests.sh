079938c421b764768c85bacffac79fac1631c06f
Lucas Walgren <lwalgren12@gmail.com>
2020-04-29 13:10:25 -0600

    fixed runtests to not kill if process doesn't exist

--- runtests.sh
+++ runtests.sh
@@ -6,7 +6,11 @@ filename="main.py"
 
 export FLASK_APP="$filename"
 
-kill "$(ps axo args,pid | grep "python3 -m flask" | cut -d ' ' -f7)"
+pid="$(ps axo args,pid | grep "python3 -m flask" | cut -d ' ' -f6)"
+if [ "$pid" != '' ]; then
+	kill "$pid"
+fi
+
 rm "./solutions/curl_scripts/final.json" 
 python3 -m flask run --host localhost >output.log 2>&1 &
 sleep 1
@@ -14,4 +18,8 @@ pytest="$(python3 -m pytest)"
 echo "$pytest"
 assert_raises "echo "$pytest" | grep "failed"" 1
 sleep 1
-kill "$(ps axo args,pid | grep "python3 -m flask" | cut -d ' ' -f7)"
+
+pid="$(ps axo args,pid | grep "python3 -m flask" | cut -d ' ' -f6)"
+if [ "$pid" != '' ]; then
+	kill "$pid"
+fi
