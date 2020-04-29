e95ee4e133b47c7bd77bb443b081597d546cd04d
Lucas Walgren <lwalgren12@gmail.com>
2020-04-29 12:46:22 -0600

    added better method of killing a running server

--- runtests.sh
+++ runtests.sh
@@ -6,7 +6,7 @@ filename="main.py"
 
 export FLASK_APP="$filename"
 
-kill "$(ps -aux | grep "python3 -m flask run" | cut -d ' ' -f 5 | head -n 1)"
+kill "$(ps axo args,pid | grep "python3 -m flask" | cut -d ' ' -f7)"
 rm "./solutions/curl_scripts/final.json" 
 python3 -m flask run --host localhost >output.log 2>&1 &
 sleep 1
@@ -14,4 +14,4 @@ pytest="$(python3 -m pytest)"
 echo "$pytest"
 assert_raises "echo "$pytest" | grep "failed"" 1
 sleep 1
-kill "$(ps -aux | grep "python3 -m flask run" | cut -d ' ' -f 5 | head -n 1)"
+kill "$(ps axo args,pid | grep "python3 -m flask" | cut -d ' ' -f7)"
