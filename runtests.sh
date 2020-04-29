3200503d7810d78dccd11ed24959af92f0f9ecff
Lucas Walgren <lwalgren12@gmail.com>
2020-04-29 17:22:14 -0600

    fixed pid lookup

--- runtests.sh
+++ runtests.sh
@@ -6,9 +6,9 @@ filename="main.py"
 
 export FLASK_APP="$filename"
 
-pid="$(ps axo args,pid | grep "python3 -m flask" | cut -d ' ' -f6)"
+pid="$(ps axo args,pid | grep "python3 -m flask" | egrep -o "[0-9]{2,}")"
 if [ "$pid" != '' ]; then
-	kill "$pid"
+	kill $pid;
 fi
 
 rm "./solutions/curl_scripts/final.json" 
@@ -16,10 +16,10 @@ python3 -m flask run --host localhost >output.log 2>&1 &
 sleep 1
 pytest="$(python3 -m pytest)"
 echo "$pytest"
-assert_raises "echo "$pytest" | grep "failed"" 1
+assert "echo "$pytest" | grep "failed"" ""
 sleep 1
 
-pid="$(ps axo args,pid | grep "python3 -m flask" | cut -d ' ' -f6)"
+pid="$(ps axo args,pid | grep "python3 -m flask" | egrep -o "[0-9]{2,}")"
 if [ "$pid" != '' ]; then
-	kill "$pid"
+	kill $pid;
 fi
