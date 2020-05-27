dbaef1737b9d61cef2f16dfe605030197df57e26
James Halladay <jehalladay@mavs.coloradomesa.edu>
2020-05-27 16:26:12 -0600

    added the ability to declare debug and host ip for testing and development

--- run.py
+++ run.py
@@ -29,8 +29,8 @@ Conventions followed:
         '''
             contents of docstring
         '''
-    Type Annotations follow the convention
-        variableName: type = value
+    Type Annotations follow the convention:
+        variableName: variableType = variableValue
     Inline Annotations follow the convention:
         def functionName(param1: paramType, param2: paramType...) -> returnType:
 """
